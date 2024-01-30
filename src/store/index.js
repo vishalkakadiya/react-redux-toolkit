1964c314cb7d31f956237cf2ffc1f33afd7139a2
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2024-01-30 09:45:44 +0530

    Use Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,23 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment(state, action) {
+            state.counter = state.counter + action.payload;
+        },
+        decrement(state) {
+            state.counter = state.counter - action.payload;
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
 
 const counterReducer = (state = initialState, action) => {
 
