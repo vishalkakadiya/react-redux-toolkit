c81a5872fe23d90eb54b22ac3f8c45f2eeedc06d
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2024-01-30 09:50:38 +0530

    Connect reducer to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore } from "redux";
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
@@ -19,32 +18,13 @@ const counterSlice = createSlice({
     }
 });
 
-const counterReducer = (state = initialState, action) => {
-
-    if ('increment' === action.type) {
-        return {
-            counter: state.counter + action.payload,
-            showCounter: state.showCounter
-        };
-    }
-
-    if ('decrement' === action.type) {
-        return {
-            counter: state.counter - action.payload,
-            showCounter: state.showCounter
-        };
-    }
-
-    if ('toggle' === action.type) {
-        return {
-            counter: state.counter,
-            showCounter: !state.showCounter
-        }
-    }
-
-    return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+    reducer: counterSlice.reducer
+    // NOTE: Below code, if you have multiple reducers.
+    // reducer: {
+    //     count: counterSlice.reducer,
+    //     other: other.reducer
+    // }
+});
 
 export default store;
